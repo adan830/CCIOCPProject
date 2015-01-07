8bdfb0a11f1a4db47b2b14e275d7df88368b5863
东日 <2429566403@qq.com>
2015-01-07 21:33:19 +0800

    ..
    ..

--- AuthenServer/AuthenServer/CAuthFailLog.cpp
+++ AuthenServer/AuthenServer/CAuthFailLog.cpp
@@ -27,12 +27,33 @@ CAuthFailFileLog::CAuthFailFileLog() : m_ulLastWriteTick(0), m_iCacheLen(0)
 CAuthFailFileLog::~CAuthFailFileLog()
 {
 	WaitThreadExecuteOver();
-
+	WriteCache;
+	free(m_pCache);
 }
 
 void CAuthFailFileLog::DoExecute()
 {
-
+	while (!IsTerminated())
+	{
+		try
+		{
+			/*
+			  ToDay := Trunc(Now());
+			  if (ToDay <> m_LastDay) then
+			  begin
+				m_LastDay := ToDay;
+				WriteCache;
+			  end;
+			  if (m_CacheLen >= MAX_FILE_CACHE_SIZE) or
+				(GetTickCount > m_LastWriteTick + WRITE_INTERVAL) then
+				WriteCache;
+			*/
+		}
+		catch (...)
+		{
+		}
+		WaitForSingleObject(m_Event, 20);
+	}
 }
 
 void CAuthFailFileLog::WriteLog(const std::string& sLog)
